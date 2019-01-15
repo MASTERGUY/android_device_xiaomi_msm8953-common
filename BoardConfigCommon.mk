10d5bb85952e581930e2fadc6d0bca7c0dfa5be3
Bruno Martins <bgcngm@gmail.com>
2019-01-15 00:24:17 +0000

    msm8953-common: Replace KeyDisabler class with Touch HAL
    Change-Id: I7b87ae0ad834ba02a78696afe393d9d4f8920fbd

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -155,6 +155,9 @@ DEVICE_MATRIX_FILE   := $(VENDOR_PATH)/compatibility_matrix.xml
 TARGET_INIT_VENDOR_LIB := libinit_msm8953
 TARGET_RECOVERY_DEVICE_MODULES := libinit_msm8953
 
+# Keymaster
+TARGET_PROVIDES_KEYMASTER := true
+
 # Lights
 TARGET_PROVIDES_LIBLIGHT := true
 
