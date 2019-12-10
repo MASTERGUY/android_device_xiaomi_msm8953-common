837b598f7cf67bf736389a8e017373e76a5f3a4e
intervigilium <intervigil@gmail.com>
2019-12-11 01:17:09 +0530

    msm8953-common: add basic usb hals that report no status change

--- msm8953.mk
+++ msm8953.mk
@@ -359,7 +359,7 @@ PRODUCT_PACKAGES += \
 
 # USB HAL
 PRODUCT_PACKAGES += \
-    android.hardware.usb@1.0-service.basic
+    android.hardware.usb@1.0-service.xiaomi_8996
 
 # VNDK
 PRODUCT_PACKAGES += \
