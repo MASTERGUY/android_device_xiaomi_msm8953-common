5c1ccc81be6e1ba1a5904a722bbe31b0132c5d81
jhenrique09 <jhenrique09.mcz@hotmail.com>
2020-07-21 15:10:39 +0000

    msm8953-common: Introduce 'SafailNet'

--- rootdir/Android.mk
+++ rootdir/Android.mk
@@ -92,3 +92,10 @@ LOCAL_SRC_FILES    := etc/move_wifi_data.sh
 LOCAL_MODULE_PATH  := $(TARGET_OUT_EXECUTABLES)
 include $(BUILD_PREBUILT)
 
+include $(CLEAR_VARS)
+LOCAL_MODULE       := init.safailnet.rc
+LOCAL_MODULE_TAGS  := optional
+LOCAL_MODULE_CLASS := ETC
+LOCAL_SRC_FILES    := etc/init.safailnet.rc
+LOCAL_MODULE_PATH  := $(TARGET_OUT_ETC)/init
+include $(BUILD_PREBUILT)
