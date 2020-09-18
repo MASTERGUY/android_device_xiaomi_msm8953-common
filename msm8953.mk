df89933a70f3827794dcd995dcfd7c5cb5f53f96
SagarMakhar <sagarmakhar@gmail.com>
2020-09-18 05:05:19 +0000

    msm8953-common: Add Prebuilts VNDK29 Protobuff Libs

--- msm8953.mk
+++ msm8953.mk
@@ -272,6 +272,11 @@ PRODUCT_PACKAGES += \
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/powerhint.xml:$(TARGET_COPY_OUT_VENDOR)/etc/powerhint.xml
 
+#Prebuilts
+PRODUCT_COPY_FILES += \
+    prebuilts/vndk/v29/arm64/arch-arm64-armv8-a/shared/vndk-core/libprotobuf-cpp-lite.so:$(TARGET_COPY_OUT_VENDOR)/lib64/libprotobuf-cpp-lite-v29.so \
+    prebuilts/vndk/v29/arm64/arch-arm64-armv8-a/shared/vndk-core/libprotobuf-cpp-full.so:$(TARGET_COPY_OUT_VENDOR)/lib64/libprotobuf-cpp-full-v29.so
+
 # Properties
 -include device/xiaomi/msm8953-common/prop.mk
 
