c5f17bab658fa1bf5ca5d43ef3d5b52c547f6ea2
MASTERGUY <murtipipo@gmail.com>
2020-11-21 06:04:51 +0000

    msm8953-common: remove proton clang defination also move product board flags in msm8953.mk
    Signed-off-by: MASTERGUY <murtipipo@gmail.com>

--- msm8953.mk
+++ msm8953.mk
@@ -19,6 +19,10 @@ $(call inherit-product, vendor/xiaomi/msm8953-common/msm8953-common-vendor.mk)
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += $(LOCAL_PATH)/overlay
 
+# Board
+PRODUCT_USES_QCOM_HARDWARE := true
+PRODUCT_BOARD_PLATFORM := msm8996
+
 # Screen density
 PRODUCT_AAPT_CONFIG := normal
 PRODUCT_AAPT_PREF_CONFIG := xxhdpi
